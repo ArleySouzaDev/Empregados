0a29bc421d001e81531638f61281b20c797d7a29
Arley Souza <souzaarley12@gmail.com>
2023-04-15 17:05:04 -0300

    att funcionários

---
+++ Funcionarios/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace Funcionarios.Entities
+{
+    internal class Employee
+    {
+        public string name { get; set; }
+        public int Hours { get; set; }
+        public int ValuePerHour { get; set; }
+
+        public Employee(string  name, int hours, double valuePerhour)
+        {
+            name = name;
+            Hours = hours;
+            ValuePerHour = ValuePerHour;
+
+        }
+
+        public double Payment()
+        {
+            return Hours *ValuePerHour;
+        }
+    }
+}
